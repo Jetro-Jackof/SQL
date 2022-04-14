a0b00cff4c5880a04dca3cdac2fa16320173c992
Jetro-Jackof <73573217+Jetro-Jackof@users.noreply.github.com>
2022-04-13 21:43:52 -0500

    finish query 4

--- olympic_games/query_4-5.sql
+++ olympic_games/query_4-5.sql
@@ -1,4 +1,22 @@
 select * from athlete_events
 #Query4
 # SQL query to return the Olympic Games which had the highest participating countries and the lowest participating countries.
-select max(count(Name)), min(count(Name)) from athlete_events
+select distinct Games, count(distinct NOC) as total
+from athlete_events
+group by Games
+order by total desc
+
+select
+concat(first_value(Games) over(order by total), ' - ', first_value(total) over(order by total)) as lowest_countries,
+concat(first_value(Games) over(order by total desc), ' - ', first_value(total) over(order by total desc)) as highest_countries
+from(
+select distinct Games, count(distinct NOC) as total
+from athlete_events
+group by Games
+) sub
+order by 1
+limit 1
+
+#Query 5
+#return the list of countries who have been part of every Olympics games
+
